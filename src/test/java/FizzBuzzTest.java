b703fc199f45b1cea2e0b1fbaa49e57c4667be62
alfredo.gil <alfredo.gil@oramon.nl>
2017-05-17 09:34:06 +0200

    KATA-FizzBuzz - Implementing failing test SayTwo

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -17,8 +17,10 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     private static final String EXPECTED_ONE="1";
+    private static final String EXPECTED_TWO="1";
 
     private static final String POINTED_WITH_ONE ="1";
+    private static final String POINTED_WITH_TWO ="2";
 
     @Test
     public void sayNumber_WhenYouArePointedWithNumberOne_YouShouldDayOne()
@@ -30,5 +32,15 @@ public class FizzBuzzTest {
         String result=SUT.sayNumber(POINTED_WITH_ONE);
 
         assertEquals("You should say one",expected,result);
+    }
+
+    @Test
+    public void sayNumber_WhenYouArePointedWithTwo_YouShouldSayTwo() {
+        final String expected = EXPECTED_TWO;
+
+        FizzBuzz SUT = new FizzBuzz();
+        String result = SUT.sayNumber(POINTED_WITH_TWO);
+
+        Assert.assertEquals("You should say two", expected, result);
     }
 }
